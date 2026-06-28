66e8a6a4be893accf40ef57fffc560f6c90b65d1
zeroglicthplayz-design <zeroglicthplayz@gmail.com>
2026-06-28 09:08:09 +0100

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Installing dependencies..."
-pip install --upgrade pip
-pip install -r requirements.txt
-pip install Flask
-
-echo "Dependencies installed successfully!"
